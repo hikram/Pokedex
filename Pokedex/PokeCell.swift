ac372c18647cc01a8fdf377b13b64a0884ecee46
Hashim Ikram <Hashim@Hashims-MacBook-Pro.local>
2016-10-10 23:27:02 +0500

    did some stuff

---
+++ Pokedex/PokeCell.swift
@@ -0,0 +1,24 @@
+//
+//  PokeCell.swift
+//  Pokedex
+//
+//  Created by Hashim Ikram on 10/10/2016.
+//  Copyright © 2016 Hashim Ikram. All rights reserved.
+//
+
+import UIKit
+
+class PokeCell: UICollectionViewCell {
+    
+    @IBOutlet weak var thumbImg: UIImageView!
+    @IBOutlet weak var nameLbl: UILabel!
+    
+    var pokemon: Pokemon!
+    
+    func configureCell(pokemon: Pokemon) {
+        self.pokemon = pokemon
+        nameLbl.text = self.pokemon.name.capitalized
+        thumbImg.image = UIImage(named: "\(self.pokemon.pokedexID)")
+    }
+    
+}
